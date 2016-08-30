b5a7ea160c9aadfcfa46215fa9c28832a596bd05
antonbelyankin <antonbelyankin>
2016-08-30 06:26:07 -0600

    ball task

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+//SELECT e.* FROM `employee` as e left join `employee` as chiefs ON chiefs.id = e.chief_id where chiefs.salary < e.salary 
+// SELECT e.* FROM `employee` as e left join `department` as d ON d.id = e.department_id where e.salary = (select max(emp.salary) from employee as emp where e.department_id = emp.department_id)
 
-document.getElementById("bagua-table").onclick  =   function(event){  
-    var tag =   event.target.tagName;
-    var parent    =   event.target;
-   
-    if(tag!=="TD")
-    {
-            while("TD"!== parent.tagName)
-            {      
-                    parent  =   parent.parentNode;
-            }
-    }
-   else
-   {
-        parent    =   event.target;
-   };
-    parent.style.backgroundColor = "red";
+document.getElementById("field").onclick    =   function(event)
+{
+        var fieldCoord   =   this.getBoundingClientRect();
+        var ball = document.getElementById("ball");
+
+        var field   =   {
+            X:   fieldCoord.left + this.clientLeft,
+            Y:   fieldCoord.top +  this.clientTop
+        };
+        
+        var ballCoords  =   {
+            top: event.clientY - field.Y - (ball.clientHeight/2) + "px",
+            left: event.clientX - field.X - (ball.clientWidth/2) +  "px"
+        };
+        
+        ball.style.top = ballCoords.top;
+        ball.style.left = ballCoords.left;
 };
 
